e56111a7764f155aa699581627e8b49500654f79
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-09-23 03:27:56 +0700

    package: plugins for vanilla extract & mdx

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
+
+import {createVanillaExtractPlugin} from '@vanilla-extract/next-plugin';
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx$/,
+});
+const withVanillaExtract = createVanillaExtractPlugin();
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  short: true,
+};
 
-module.exports = nextConfig
+module.exports = withVanillaExtract(withMDX(nextConfig));
