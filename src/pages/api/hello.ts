0d5130fc04514a118b89a9213fc5a3304bb8a72c
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-09-23 15:07:02 +0700

    chore(hello.ts): added types

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,5 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type {NextApiRequest, NextApiResponse} from 'next';
 
-export default function handler(req, res) {
-  res.status(200).json({name: 'John Doe'});
+import {readdir} from 'fs/promises';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+
+  res.status(200).json({files});
 }
