434ac45b5fd1d53475e240e3609bb167578f0d5c
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-19 15:14:44 +0700

    fix: responsive shenaniganas

--- src/pages/index.css.ts
+++ src/pages/index.css.ts
@@ -1,4 +1,4 @@
-import { globalStyle, style } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 import { globalVars } from "../styles/theme.css";
 
 export const root = style({
@@ -7,26 +7,39 @@ export const root = style({
   gridTemplateRows: "minmax(100px, auto)",
   gridAutoFlow: "dense",
   gridGap: "0.5rem",
+  "@media": {
+    "(max-width: 680px)": {
+      gridTemplateColumns: "100px",
+      gridTemplateRows: "minmax(auto, auto)",
+    },
+  },
 });
 
-export const cardBase = style({
+const cardBase = style({
   background: "hsla(0, 0%, 30%, 10%)",
   boxShadow: "0px 2px 5px rgba(0,0,0,0.25)",
   backdropFilter: "blur(1.1px)",
+  borderRadius: "16px",
   padding: "20px",
   cursor: "pointer",
   ":active": {
     opacity: 0.85,
   },
   ":first-child": {
-    backgroundColor: globalVars.colors.blue,
-    color: globalVars.colors.white,
+    backgroundColor: globalVars.colors.green,
+    color: globalVars.colors.black,
   },
   "@media": {
     "(prefers-color-scheme: dark)": {
       ":first-child": {
         backgroundColor: globalVars.colors.green,
         color: globalVars.colors.black,
+      },
+    },
+    "(prefers-color-scheme: light)": {
+      ":first-child": {
+        backgroundColor: globalVars.colors.blue,
+        color: globalVars.colors.white,
       },
     },
   },
@@ -40,6 +53,11 @@ const cardGrid = style({
       gridColumn: "span 2",
     },
     "&:nth-child(6n)": {
+      gridColumn: "span 3",
+    },
+  },
+  "@media": {
+    "(max-width: 680px)": {
       gridColumn: "span 3",
     },
   },
--- src/styles/global.css.ts
+++ src/styles/global.css.ts
@@ -5,20 +5,29 @@ globalFontFace("Open Sans", {
   src: 'local("Open Sans")',
 });
 
-//const grainy = `linear-gradient(0deg, rgba(0,0,0,0.83), rgba(0,0,0,0.72)),
-//url("data:image/svg+xml,%3Csvg viewBox='0 0 224 224' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='10' numOctaves='5' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E")`;
-
 globalStyle("html, body", {
   fontFamily:
     "-apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif",
   margin: 0,
   padding: 0,
+  background: globalVars.colors.grayish,
+  color: globalVars.colors.white,
   "@media": {
+    "(prefers-color-scheme: light)": {
+      background: globalVars.colors.white,
+      color: globalVars.colors.black,
+    },
     "(prefers-color-scheme: dark)": {
       background: globalVars.colors.grayish,
-      color: globalVars.colors["bg-light"],
+      color: globalVars.colors.white,
     },
   },
+});
+
+// just in case
+globalStyle("body.dark", {
+  background: globalVars.colors.grayish,
+  color: globalVars.colors["bg-light"],
 });
 
 globalStyle("body::-webkit-scrollbar", {
