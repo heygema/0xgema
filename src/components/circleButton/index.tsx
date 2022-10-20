90a389ccc7071554e86c34b7c36d392a837b4812
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-21 05:40:55 +0700

    chore: added modal init

--- src/components/circleButton/index.tsx
+++ src/components/circleButton/index.tsx
@@ -1,24 +1,62 @@
 import { motion } from "framer-motion";
 import * as styles from "./cirlceButton.css";
+import "reactjs-popup/dist/index.css";
+import Popup from "reactjs-popup";
+import { useEffect, useState } from "react";
 
 interface Props {
   onClick?: () => void;
 }
 
 export default function CircleButton({ onClick }: Props) {
+  const [open, setOpen] = useState(false);
+
+  const closeModal = () => setOpen(false);
+
+  useEffect(() => {
+    let keyHandler: ReturnType<typeof document.addEventListener> | undefined;
+    if (document !== undefined) {
+      keyHandler = document.addEventListener("keydown", (event) => {
+        console.log(event.key);
+        switch (event.key) {
+          case "k": {
+            if (event.metaKey) {
+              setOpen(() => true);
+            }
+          }
+        }
+      });
+    }
+
+    // fuck
+    return () => removeEventListener("keydown", keyHandler as any);
+  }, []);
+
   return (
-    <motion.div
-      onClick={onClick}
-      aria-label="menu-button"
-      whileHover={{
-        scale: 1.2,
-      }}
-      whileTap={{
-        scale: 0.9,
-      }}
-      className={styles.container}
+    <Popup
+      onClose={closeModal}
+      closeOnEscape
+      closeOnDocumentClick
+      trigger={
+        <motion.div
+          onClick={onClick}
+          aria-label="menu-button"
+          whileHover={{
+            scale: 1.2,
+          }}
+          whileTap={{
+            scale: 0.9,
+          }}
+          className={styles.container}
+        >
+          <div className={styles.circleFallback} />
+        </motion.div>
+      }
+      open={open}
+      modal
+      position="right center"
     >
-      <div className={styles.circleFallback} />
-    </motion.div>
+      <div>Popup content here !!</div>
+    </Popup>
   );
 }
