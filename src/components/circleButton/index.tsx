95a42f373e632a19362e807674e4989f6c0e74f8
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-21 04:44:33 +0700

    chore: nit style

--- src/components/circleButton/index.tsx
+++ src/components/circleButton/index.tsx
@@ -1,12 +1,17 @@
 import { motion } from "framer-motion";
 import * as styles from "./cirlceButton.css";
 
-export default function CircleButton() {
+interface Props {
+  onClick?: () => void;
+}
+
+export default function CircleButton({ onClick }: Props) {
   return (
     <motion.div
+      onClick={onClick}
       aria-label="menu-button"
       whileHover={{
-        scale: 1.1,
+        scale: 1.2,
       }}
       whileTap={{
         scale: 0.9,
