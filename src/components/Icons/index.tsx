e7ec76193b0c29335e1eee68c8a164c8ce51bf28
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-24 22:42:32 +0700

    chore: more icons

--- src/components/Icons/index.tsx
+++ src/components/Icons/index.tsx
@@ -1,11 +1,20 @@
 import { CiTwitter as Twitter } from "react-icons/ci";
-import { GrHomeRounded as Home } from "react-icons/gr";
+import {
+  GrPersonalComputer as System,
+  GrHomeRounded as Home,
+} from "react-icons/gr";
+
 import { VscGithubAlt as Github } from "react-icons/vsc";
 import { RiSkullLine as About } from "react-icons/ri";
+import { BiMoon as Night } from "react-icons/bi";
+import { BsSun as Sun } from "react-icons/bs";
 
 export const Icons = {
   Home,
   About,
   Twitter,
   Github,
+  System,
+  Night,
+  Sun,
 };
