c144a1d7b822ff77198babe8015c75c9c6036260
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-28 18:32:32 +0700

    fix: imports

--- src/components/BioSection/index.tsx
+++
@@ -1,6 +0,0 @@
-import { ReactNode } from "react";
-import { Card } from "../../core-ui/Card";
-
-export function BioSection({ children }: { children: ReactNode }) {
-  return <Card variant="bio">{children}</Card>;
-}
--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export * from "./CircleMenu";
 export * from "./CoolLayout";
 export * from "./ThemeChanger";
 export * from "./Icons";
-export * from "./Card";
