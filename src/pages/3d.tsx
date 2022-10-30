71a51b48006a651e2178691c5e36fe94710b4960
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-31 01:28:48 +0700

    chore: few scene

---
+++ src/pages/3d.tsx
@@ -0,0 +1,38 @@
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Canvas, ThreeElements } from "@react-three/fiber";
+import { Suspense, useRef } from "react";
+import { globalVars } from "../styles/theme.css";
+
+function Model({ url }) {
+  const gltf = useGLTF(url, true);
+  return <primitive object={gltf.scene} dispose={null} />;
+}
+
+export default function D() {
+  return (
+    <div
+      style={{
+        height: "300px",
+        width: "300px",
+        borderRadius: "100%",
+        border: `15px solid ${globalVars.colors["grayish-02"]}`,
+        margin: "0 auto",
+      }}
+    >
+      <Canvas
+        style={{
+          borderRadius: "100%",
+        }}
+        className=""
+      >
+        <Suspense fallback={"Loading..."}>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <mesh rotation={[-0.15, -0.1, 0]} position={[20, -290, -360]}>
+            <Model url="/assets/models/toon_cat_free/scene.gltf" />
+          </mesh>
+        </Suspense>
+      </Canvas>
+    </div>
+  );
+}
