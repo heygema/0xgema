91f70a755f4bcd0fffaf07ce7088df95532d3e3b
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-10-31 14:17:41 +0700

    chore: a target blank

--- src/components/MDX/index.tsx
+++ src/components/MDX/index.tsx
@@ -2,6 +2,8 @@ import * as styles from "./style.css";
 
 export const MDXComponents = {
   p: (props: any) => <p {...props} />,
-  a: (props: any) => <a className={styles.link} {...props} />,
-  blockquote: (props: any) => <blockquote className={styles.blockquote} {...props} />,
+  a: (props: any) => <a target="_blank" className={styles.link} {...props} />,
+  blockquote: (props: any) => (
+    <blockquote className={styles.blockquote} {...props} />
+  ),
 };
