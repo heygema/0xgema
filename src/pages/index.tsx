9cfff002dc8bbec8e1bad8237cd9c6486ae52095
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-11-01 17:00:15 +0700

    chore: under construction

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -130,6 +130,7 @@ export default function Home({ posts }: Props) {
 
   return (
     <Suspense fallback={<Loading />}>
+      <h2> Like this site is bad, it's under construction 😂</h2>
       <Link href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
         <div className={styles.easterEggContainer}>
           <span className={styles.eggUnicorn}>🥚🦄</span>
