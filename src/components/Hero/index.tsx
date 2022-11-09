25256c212caf2c19dabdfb2917ee447558f166d7
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-11-09 14:48:10 +0700

    fix: hydration problem

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,16 +2,15 @@ import { useTime } from "../../hooks/useTime";
 import * as styles from "./style.css";
 
 export function Hero() {
-  const { hour, minute, second } = useTime();
+  const time = useTime();
+
+  const clock = time && `${time.hour}:${time.minute}:${time.second}`;
 
   return (
     <div className={styles.root}>
       <h4 className={styles.title}>Gema Anggada</h4>
       <p>
-        Software Engineer,{" "}
-        <span>
-          (UTC+7) {hour}:{minute}:{second}
-        </span>
+        Software Engineer, <span>(UTC+7) {clock}</span>
       </p>
     </div>
   );
--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -6,20 +6,27 @@ const f = (n: number) => {
   return `0${n}`.slice(-2);
 };
 
-const getTime = () => {
-  let { hour, minute, second } = DateTime.now().setZone("UTC+7");
-
-  return { hour: f(hour), minute: f(minute), second: f(second) };
+const getTime = (hydrated?: boolean) => {
+  if (hydrated) {
+    let { hour, minute, second } = DateTime.now().setZone("UTC+7");
+    return { hour: f(hour), minute: f(minute), second: f(second) };
+  }
+  return;
 };
 
 export function useTime() {
+  const [hydrated, setHydrated] = useState(false);
   const [time, setTime] = useState(() => {
-    return getTime();
+    return getTime(hydrated);
   });
 
   useEffect(() => {
+    setHydrated(true);
+  }, []);
+
+  useEffect(() => {
     let timeout = setTimeout(() => {
-      setTime(getTime());
+      setTime(getTime(hydrated));
     }, 1000);
 
     return () => {
