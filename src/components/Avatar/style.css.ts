9664ada2a91d394346879a941d35700c530b2a63
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2022-11-25 14:29:26 +0700

    update avatar

--- src/components/Avatar/style.css.ts
+++ src/components/Avatar/style.css.ts
@@ -1,12 +1,12 @@
 import { style } from "@vanilla-extract/css";
 
 export const root = style({
-  height: "200px",
-  background: "url(/assets/images/942.jpeg)",
-  backgroundPosition: "center",
-  backgroundSize: "cover",
-  width: "200px",
-  margin: "0 auto",
-  borderRadius: "16px",
-  //borderRadius: "100%",
+    background: "url(/assets/images/942.jpeg)",
+    backgroundPosition: "center",
+    backgroundSize: "cover",
+    width: "150px",
+    aspectRatio: "1 / 1",
+    //margin: "0 auto",
+    borderRadius: "16px",
+    //borderRadius: "100%",
 });
