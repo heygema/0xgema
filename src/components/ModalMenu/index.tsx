458d5c35d945b0b0ad357933891a47f61fefd4d5
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2023-03-09 01:44:47 +0700

    try inputMode = none

--- src/components/ModalMenu/index.tsx
+++ src/components/ModalMenu/index.tsx
@@ -261,6 +261,7 @@ export default function ModalMenu() {
     >
       <div className={styles.searchContainer}>
         <input
+          inputMode="none"
           aria-label="global-search-box"
           className={styles.inputStyle}
           placeholder="Search for posts and menus..."
