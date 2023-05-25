885cecef98eef3c4a3bf0a1191857287df02c980
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2023-05-25 07:42:03 +0700

    add shadow

--- src/components/Avatar/style.css.ts
+++ src/components/Avatar/style.css.ts
@@ -1,4 +1,5 @@
 import {style} from '@vanilla-extract/css';
+import { globalVars } from '../../styles/theme.css';
 
 export const root = style({
   background: 'url(/assets/images/avatar-bg-white.png)',
@@ -7,6 +8,7 @@ export const root = style({
   width: '50px',
   aspectRatio: '1 / 1',
   cursor: 'pointer',
+  boxShadow: globalVars.shadow.macShadow,
   // margin: '0 auto',
   //borderRadius: "16px",
   borderRadius: '100%',
