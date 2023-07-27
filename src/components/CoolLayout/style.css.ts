d1a07541bfa4f01d5b5da9ba43fd00f273a7b99c
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2023-07-27 12:22:19 +0700

    chore: smaller footer

--- src/components/CoolLayout/style.css.ts
+++ src/components/CoolLayout/style.css.ts
@@ -1,63 +1,64 @@
-import { style, styleVariants } from "@vanilla-extract/css";
-import { sprinkles } from "../../styles/sprinkles.css";
-import { globalVars } from "../../styles/theme.css";
+import { style, styleVariants } from '@vanilla-extract/css';
+import { sprinkles } from '../../styles/sprinkles.css';
+import { globalVars } from '../../styles/theme.css';
 
 export const navigation = style({
-  width: "100%",
+  width: '100%',
   top: 0,
-  height: "4rem",
+  height: '4rem',
 });
 
 const mainBase = style({
-  margin: "0 auto",
-  marginTop: "10vmin",
-  marginBottom: "5rem",
+  margin: '0 auto',
+  marginTop: '10vmin',
+  marginBottom: '5rem',
 });
 
 const mainResponsive = sprinkles({
   width: {
-    desktop: "breakpoint-desktop",
-    tablet: "breakpoint-tablet",
-    mobile: "breakpoint-mobile",
-    ["mobile-s"]: "breakpoint-x-small",
+    desktop: 'breakpoint-desktop',
+    tablet: 'breakpoint-tablet',
+    mobile: 'breakpoint-mobile',
+    ['mobile-s']: 'breakpoint-x-small',
   },
 });
 
 export const menuContainer = style([
   style({
-    margin: "0 auto",
+    margin: '0 auto',
     paddingTop: 63,
-    paddingRight: "5vmin",
-    height: "5rem",
-    display: "flex",
+    paddingRight: '5vmin',
+    height: '5rem',
+    display: 'flex',
     //justifyContent: "flex-end",
     // experiment on the center
-    justifyContent: "flex-end",
-    flexDirection: "row",
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
   }),
   mainResponsive,
 ]);
 
 export const footer = style([
   style({
-    margin: "0 auto",
-    height: "3rem",
-    display: "flex",
-    justifyContent: "center",
-    flexDirection: "row",
+    margin: '0 auto',
+    height: '3rem',
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    fontSize: '0.8rem',
   }),
   mainResponsive,
 ]);
 
 export const backGlow = style({
-  position: "absolute",
+  position: 'absolute',
   background: globalVars.colors.gradientPastelRainbow,
-  aspectRatio: "1 / 1",
-  borderRadius: "100%",
-  height: "300px",
-  filter: "blur(90px)",
+  aspectRatio: '1 / 1',
+  borderRadius: '100%',
+  height: '300px',
+  filter: 'blur(90px)',
   opacity: 0.2,
-  pointerEvents: "none",
+  pointerEvents: 'none',
 });
 
 export const backGlowVariant = styleVariants({
@@ -65,7 +66,7 @@ export const backGlowVariant = styleVariants({
   bottom: [
     backGlow,
     {
-      height: "100px",
+      height: '100px',
       bottom: 1,
       right: 1,
     },
