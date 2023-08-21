79e68d11bc37007441e4405179e76e1b210abca2
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2023-08-21 07:44:31 +0700

    smaller avatar

--- src/components/Avatar/style.css.ts
+++ src/components/Avatar/style.css.ts
@@ -5,7 +5,7 @@ export const root = style({
   background: 'url(/assets/images/avatar-bg-white.png)',
   backgroundPosition: 'center',
   backgroundSize: 'cover',
-  width: '50px',
+  width: '45px',
   aspectRatio: '1 / 1',
   cursor: 'pointer',
   // margin: '0 auto',
