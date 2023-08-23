6110fe18e9a86762ddadba351e4ac82e1a728ed1
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2023-08-24 01:56:05 +0700

    formatting

--- src/pages/3d.tsx
+++ src/pages/3d.tsx
@@ -1,7 +1,7 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import { Canvas, ThreeElements } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
-import { globalVars } from "../styles/theme.css";
+import { OrbitControls, useGLTF } from '@react-three/drei';
+import { Canvas, ThreeElements } from '@react-three/fiber';
+import { Suspense, useRef } from 'react';
+import { globalVars } from '../styles/theme.css';
 
 function Model({ url }) {
   const gltf = useGLTF(url, true);
@@ -12,23 +12,23 @@ export default function D() {
   return (
     <div
       style={{
-        height: "300px",
-        width: "300px",
-        borderRadius: "100%",
-        border: `15px solid ${globalVars.colors["grayish-02"]}`,
-        margin: "0 auto",
+        height: '300px',
+        width: '300px',
+        borderRadius: '100%',
+        border: `15px solid ${globalVars.colors['grayish-02']}`,
+        margin: '0 auto',
       }}
     >
       <Canvas
         style={{
-          borderRadius: "100%",
+          borderRadius: '100%',
         }}
         className=""
         camera={{
           position: [0, 0, 0],
         }}
       >
-        <Suspense fallback={"Loading..."}>
+        <Suspense fallback={'Loading...'}>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
           <mesh rotation={[-0.15, -0.1, 0]} position={[20, -290, -360]}>
