c34f9b81c89a63946ada26095552829bd2ca702e
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2025-02-01 14:06:10 +0700

    changes to avatar image

--- src/components/Avatar/style.css.ts
+++ src/components/Avatar/style.css.ts
@@ -1,17 +1,17 @@
-import { globalStyle, style } from '@vanilla-extract/css';
-import { globalVars } from '../../styles/theme.css';
+import { globalStyle, style } from "@vanilla-extract/css";
+import { globalVars } from "../../styles/theme.css";
 
 export const root = style({
-  background: 'url(/assets/images/hexagram.jpg)',
-  backgroundPosition: 'center',
-  backgroundSize: 'cover',
-  width: '45px',
-  aspectRatio: '1 / 1',
-  cursor: 'pointer',
+  background: "url(/assets/images/one-eye.gif)",
+  backgroundPosition: "center",
+  backgroundSize: "cover",
+  width: "45px",
+  aspectRatio: "1 / 1",
+  cursor: "pointer",
   // margin: '0 auto',
   //borderRadius: "16px",
-  borderRadius: '100%',
-  transition: 'box-shadow 0.2s linear',
+  borderRadius: "100%",
+  transition: "box-shadow 0.2s linear",
 });
 
 globalStyle(`${root}:hover`, {
