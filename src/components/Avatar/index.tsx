fec422709ee437db50119dec6521d6c01169a9d3
Gema Anggada ✌︎ <10743728+heygema@users.noreply.github.com>
2025-02-10 13:00:00 +0700

    chore: adds glitch

--- src/components/Avatar/index.tsx
+++ src/components/Avatar/index.tsx
@@ -1,15 +1,22 @@
+import React from 'react';
 import Link from 'next/link';
+import { PowerGlitch } from 'powerglitch';
 import * as styles from './style.css';
 import { motion } from 'framer-motion';
 import { CLICKABLE_RESPONSE_PROPS } from '../../constant';
 
 export function Avatar() {
+
+  React.useEffect(() => {
+    PowerGlitch.glitch('.glitch');
+  }, [])
+  
   return (
     <Link href="/">
       <motion.div
         {...CLICKABLE_RESPONSE_PROPS}
         title="avatar"
-        className={styles.root}
+        className={[styles.root, "glitch"].join(" ")}
       />
     </Link>
   );
